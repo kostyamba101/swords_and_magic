fce43ee460be36793f309c4ea8f6837e435ebd51
kostyamba101 <kostyamba101@gmail.com>
2024-04-28 10:30:15 +0400

    0.0.4+4

--- bin/main.dart
+++ bin/main.dart
@@ -2,41 +2,59 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   /// Меню
-  print('1.Новая игра');
-  print('2.Загрузить игру');
-  print('3.Выйти');
-
-  print('\n');
+  showMenu();
 
   print('Введите необходимый пункт: ');
   // Приводим строку к типу int
   final int menu = int.tryParse(stdin.readLineSync() ?? "0") ?? 0;
 
-  // Проверяем на правильность ввода пункта меню и выводим сообщение
-  if (menu <= 0 || menu > 3) {
+  if (checkEnter(menu)) {
     print('Вы ввели не правильный пункт меню!');
   } else if (menu == 1) {
-    // Новая игра
-
-    print('Новая игра...');
-
-    /// Имя главного героя
-    print('Введите имя главного героя:');
-    final name = stdin.readLineSync();
-
-    /// Класс главного героя
-    print('Введите название класса: ');
-    final typeHero = stdin.readLineSync();
-    print('$name Добро в мир Меча и Магии');
-    print('Ваш класс $typeHero');
-    print('Игра началась...');
+    newGame();
   } else if (menu == 2) {
-    // Если пользователь ввел пункт меню 2, то выводим сообщение о том,
-    // что игра загружается
-    print('Загрузить игру...');
+    loadGame();
   } else if (menu == 3) {
-    // Если сли пользователь ввел пункт меню 3,
-    // то выводим сообщение о том, что выходим из игры
-    print('Выходим из игры...');
+    exitGame();
   }
+}
+
+/// Вывод меню
+void showMenu() {
+  print('1.Новая игра');
+  print('2.Загрузить игру');
+  print('3.Выйти');
+
+  print('\n');
+}
+
+/// Выход из игры
+void exitGame() {
+  print('Выходим из игры...');
+}
+
+/// Загрузка игры
+void loadGame() {
+  print('Загрузить игру...');
+}
+
+/// Новая игра
+void newGame() {
+  print('Новая игра...');
+
+  /// Имя главного героя
+  print('Введите имя главного героя:');
+  final name = stdin.readLineSync();
+
+  /// Класс главного героя
+  print('Введите название класса: ');
+  final typeHero = stdin.readLineSync();
+  print('$name Добро в мир Меча и Магии');
+  print('Ваш класс $typeHero');
+  print('Игра началась...');
+}
+
+/// Проверяем на правильность ввода пункта меню
+bool checkEnter(int menu) {
+  return menu < 0 || menu > 3;
 }
